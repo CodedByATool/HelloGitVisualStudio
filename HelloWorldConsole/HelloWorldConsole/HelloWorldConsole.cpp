beda7cb2b123d421de0182b7225df9be83c7415f
CodedByATool <dsewell.nz@gmail.com>
2016-08-16 20:53:05 -0700

    Initial Hello World Project
    Testing

---
+++ HelloWorldConsole/HelloWorldConsole/HelloWorldConsole.cpp
@@ -0,0 +1,14 @@
+// HelloWorldConsole.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int main()
+{
+	char buf[10];
+	std::cout << "Hello World!" << std::endl;
+	std::cin >> buf;
+    return 0;
+}
+
